9f3c4db09afc5c49ca634659844362ebb71ee067
SunnyMcCool <muellersa71566@th-nuernberg.de>
2021-06-23 09:10:11 +0200

    deleted Tuple2 & Multiconsumer, now working

--- src/main/java/ohm/softa/a12/icndb/suppliers/AllJokesSupplier.java
+++ src/main/java/ohm/softa/a12/icndb/suppliers/AllJokesSupplier.java
@@ -14,58 +14,60 @@ import java.util.function.Supplier;
  * @author Peter Kurfer
  */
 
-public final class AllJokesSupplier implements Supplier<ResponseWrapper<JokeDto>>{
+public final class AllJokesSupplier implements Supplier<ResponseWrapper<JokeDto>> {
 
-    /* ICNDB API proxy to retrieve jokes */
-    private final ICNDBApi icndbApi;
-    private int jokeCount;
-	private int usedJokes;
-	private int jokeId;
+	/**
+	 * ICNDBApi proxy
+	 */
+	private final ICNDBApi icndbApi;
+
+	/**
+	 * total count of jokes
+	 */
+	private int jokesCount;
+
+	/**
+	 * count of already retrieved jokes, required to determine when counter has to be reset
+	 */
+	private int retrievedJokes;
 
 
+	private int latestId;
 
 	public AllJokesSupplier() {
-		jokeId = 0;
-		usedJokes=0;
-        icndbApi = ICNDBService.getInstance();
-
-        /* TODO fetch the total count of jokes the API is aware of
-		* to determine when all jokes are iterated and the counters have to be reset */
+		retrievedJokes = 0;
+		latestId = 0;
+		icndbApi = ICNDBService.getInstance();
 		try {
-			jokeCount = icndbApi.getJokeCount().get().getValue();
+			jokesCount = icndbApi.getJokeCount().get().getValue();
 		} catch (InterruptedException | ExecutionException e) {
 			/* fallback - not the best kind of solution here */
-			jokeCount = 0;
+			jokesCount = 0;
 		}
-    }
+	}
 
-    public ResponseWrapper<JokeDto> get() {
-        /* TODO retrieve the next joke
-         * note that there might be IDs that are not present in the database
-         * you have to catch an exception and continue if no joke was retrieved to an ID
-         * if you retrieved all jokes (count how many jokes you successfully fetched from the API)
-         * reset the counters and continue at the beginning */
-        //throw new NotImplementedException("Method `get()` is not implemented");
-
+	@Override
+	public ResponseWrapper<JokeDto> get() {
 		/* if fallback value return null to indicate that no jokes can be retrieved */
-		if(jokeCount == 0) return null;
+		if(jokesCount == 0) return null;
 		ResponseWrapper<JokeDto> retrievedJoke;
 		/* try ro retrieve a joke until it succeeds */
 		do {
 			try {
 				/* if all jokes were retrieved - reset counters */
-				if(usedJokes >= jokeCount) {
-					jokeId = 0;
-					usedJokes = 0;
+				if(retrievedJokes >= jokesCount) {
+					latestId = 0;
+					retrievedJokes = 0;
 				}
 				/* fetch joke with a blocking future */
-				retrievedJoke = icndbApi.getJoke(++jokeId).get();
-				usedJokes++;
+				retrievedJoke = icndbApi.getJoke(++latestId).get();
+				retrievedJokes++;
 			} catch (InterruptedException | ExecutionException e) {
 				retrievedJoke = null;
 			}
 		}while (retrievedJoke == null);
 		return retrievedJoke;
-    }
+	}
 
 }
+
